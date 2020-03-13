027b253f83f215883c6e728b34773bd909786a45
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-03-13 13:49:56 +0800

    Add incremental building of ps module

--- src/Antd/Codegen/ModuleBundler.purs
+++ src/Antd/Codegen/ModuleBundler.purs
@@ -4,45 +4,156 @@ module Antd.Codegen.ModuleBundler
 
 import Prelude
 
-import Antd.Codegen.Types (AntModule, PSDecl(..), PSDeclName(..), PSModule)
+import Antd.Codegen.Types (AntModule, Component, PSDecl(..), PSDeclName(..), PSModule, PSRecordRow, Prop, PropTyp, Typ(..))
+import Data.Array as Array
+import Data.Map (Map)
+import Data.Map as Map
 import Data.Maybe (Maybe(..))
+import Data.Set (Set)
+import Data.Set as Set
 import Data.String as String
+import Data.Tuple (uncurry)
 
 createPSModule :: AntModule -> PSModule
-createPSModule { name } =
+createPSModule am =
+  build am.primaryComponent.name $ addComponent am.primaryComponent
+
+type State =
+  { importPrelude :: Boolean
+  , exports :: Array PSDeclName
+  , imports :: Map String (Set PSDeclName)
+  , declarations :: Array PSDecl
+  }
+
+addImportPrelude :: State -> State
+addImportPrelude = _ { importPrelude = true }
+
+addImportType :: String -> String -> Boolean -> State -> State
+addImportType mod name includeConstructors =
+  addImport mod $ PSDeclNameType { name, includeConstructors }
+
+addImportFun :: String -> String -> State -> State
+addImportFun mod name =
+  addImport mod $ PSDeclNameFun name
+
+addImportClass :: String -> String -> State -> State
+addImportClass mod name =
+  addImport mod $ PSDeclNameClass name
+
+
+addImport :: String -> PSDeclName -> State -> State
+addImport mod name prev =
+  prev { imports = newImports
+       }
+  where
+    newImports = Map.alter (Just <<< updateModEntry) mod prev.imports
+
+    updateModEntry Nothing = Set.singleton name
+    updateModEntry (Just p) = Set.insert name p
+
+addExportType :: String -> Boolean -> State -> State
+addExportType name includeConstructors =
+  addExport $ PSDeclNameType { name, includeConstructors }
+
+addExportFun :: String -> State -> State
+addExportFun name =
+  addExport $ PSDeclNameFun name
+
+addExport :: PSDeclName -> State -> State
+addExport name prev =
+  prev { exports = Array.snoc prev.exports name
+       }
+
+addImportsForPropTyp :: PropTyp -> State -> State
+addImportsForPropTyp { required, typ } =
+  addImportsForTyp typ >>>
+  if not required
+  then addImportType "Untagged.Union" "UndefinedOr" false
+  else identity
+
+-- TODO add others after writing tests
+addImportsForTyp :: Typ -> State -> State
+addImportsForTyp TypInt = addImportPrelude
+addImportsForTyp _ = identity
+
+addDecls :: Array PSDecl -> State -> State
+addDecls decls prev = prev { declarations = prev.declarations <> decls }
+
+addComponent :: Component -> State -> State
+addComponent { name, props } =
+  addComponentImports
+  >>> importPropTyps
+  >>> addExport ( PSDeclNameType { name: pcn.propsName
+                                 , includeConstructors: false
+                                 }
+                )
+  >>> addExport (PSDeclNameFun pcn.funName)
+  >>> addDecls
+  [ PSDeclTypeRecord
+      { name: pcn.propsName
+      , rows: propToPSRecordRow <$> props
+      }
+  , PSDeclForeignRC pcn
+  ]
+  where
+    importPropTyps =
+      Array.foldl (\acc p -> addImportsForPropTyp p.propTyp) identity props
+
+    pcn = mkCompNames name
+
+addComponentImports :: State -> State
+addComponentImports =
+  addImportType  "React.Basic" "ReactComponent" false >>>
+  addImportFun   "React.Basic" "element" >>>
+  addImportClass "Untagged.Coercible" "Coercible" >>>
+  addImportFun   "Untagged.Coercible" "coerce"
+
+build :: String -> (State -> State) -> PSModule
+build name builderF =
   { name: "Antd." <> name
-  , exports:
-    [ PSDeclNameType { name: pcn.propsName
-                     , includeConstructors: false
-                     }
-    , PSDeclNameFun pcn.funName
-    ]
-  , importPrelude: false
-  , imports:
-    [ { mod: "React.Basic"
-      , names: [ PSDeclNameType { includeConstructors: false
-                                , name: "ReactComponent"
-                                }
-               , PSDeclNameFun "element"
-               ]
-      }
-    , { mod: "Untagged.Coercible"
-      , names: [ PSDeclNameClass "Coercible"
-               , PSDeclNameFun "coerce"
-               ]
-      }
-    ]
-  , declarations:
-    [ PSDeclTypeRecord
-              { name: pcn.propsName
-              , rows: []
-              }
-    , PSDeclForeignRC pcn
-    ]
+  , exports: finalState.exports
+  , importPrelude: finalState.importPrelude
+  , imports
+  , declarations: finalState.declarations
   }
 
   where
-    pcn = mkCompNames name
+    initState =
+      { importPrelude: false
+      , exports: mempty
+      , imports: mempty
+      , declarations: mempty
+      }
+
+    finalState = builderF initState
+
+    -- note: Map.toUnfoldable already guarantees
+    -- sorted keys
+    imports = uncurry toPSImport <$> Map.toUnfoldable finalState.imports
+
+    toPSImport mod namesSet =
+      { mod
+      , names: Set.toUnfoldable namesSet
+      }
+
+
+propToPSRecordRow :: Prop -> PSRecordRow
+propToPSRecordRow p =
+  { name: p.name
+  , propTyp: p.propTyp
+  , documentation
+  }
+  where
+    docSections = Array.catMaybes
+      [ p.docDescription
+      , ("Type: " <> _) <$> p.docType
+      , ("Default: " <> _) <$> p.docDefault
+      ]
+
+    documentation = case docSections of
+      [] -> Nothing
+      _ -> Just $ Array.intercalate "\n" docSections
+
 
 type CompNames =
   { funName :: String
--- test/Antd/Codegen/ModuleBundlerSpec.purs
+++ test/Antd/Codegen/ModuleBundlerSpec.purs
@@ -15,39 +15,58 @@ import Test.Spec.Assertions (shouldEqual)
 moduleBundlerSpec :: Spec Unit
 moduleBundlerSpec =
   describe "ModuleBundler" do
-    it "should create a ps module with no props" do
-      createFooPSModule [] `shouldEqual`
-        { name: "Antd.Foo"
-        , exports:
-          [ PSDeclNameType { name: "FooProps", includeConstructors: false }
-          , PSDeclNameFun "foo"
-          ]
-        , importPrelude: false
-        , imports:
-          [ { mod: "React.Basic"
-            , names: [ PSDeclNameType { includeConstructors: false
-                                      , name: "ReactComponent"
-                                      }
-                     , PSDeclNameFun "element"
-                     ]
-            }
-          , { mod: "Untagged.Coercible"
-            , names: [ PSDeclNameClass "Coercible"
-                     , PSDeclNameFun "coerce"
-                     ]
-            }
-          ]
-        , declarations:
-          [ PSDeclTypeRecord
+    it "should create a ps module with a proper name" do
+      emptyModule.name `shouldEqual` "Antd.Foo"
+
+    it "should create an empty ps module with minimal import" do
+      emptyModule.importPrelude `shouldEqual` false
+      emptyModule.imports `shouldEqual`
+        [ { mod: "React.Basic"
+          , names: [ PSDeclNameType { includeConstructors: false
+                                    , name: "ReactComponent"
+                                    }
+                   , PSDeclNameFun "element"
+                   ]
+          }
+        , { mod: "Untagged.Coercible"
+          , names: [ PSDeclNameClass "Coercible"
+                   , PSDeclNameFun "coerce"
+                   ]
+          }
+        ]
+
+    it "should create an empty ps module with exports" do
+      emptyModule.exports `shouldEqual`
+        [ PSDeclNameType { name: "FooProps", includeConstructors: false }
+        , PSDeclNameFun "foo"
+        ]
+
+    it "should create an empty ps module with declarations" do
+      emptyModule.declarations `shouldEqual`
+        [ PSDeclTypeRecord
               { name: "FooProps"
               , rows: []
               }
-          , PSDeclForeignRC { funName: "foo"
-                            , foreignComponentName: "_foo"
-                            , propsName: "FooProps"
-                            }
-          ]
-        }
+        , PSDeclForeignRC { funName: "foo"
+                          , foreignComponentName: "_foo"
+                          , propsName: "FooProps"
+                          }
+        ]
+
+    it "should import prelude when needed" do
+      ( createFooPSModule
+       [ prop "bar" $ requiredPropTyp TypInt
+       ]
+      ).importPrelude `shouldEqual` true
+
+    it "should import UndefinedOr when needed" do
+      ( createFooPSModule
+       [ prop "bar" $ optionalPropTyp TypInt
+       ]
+      ).imports `shouldContainImport` (importType "Untagged.Union" "UndefinedOr")
+
+emptyModule :: PSModule
+emptyModule = createFooPSModule []
 
 createFooPSModule :: Array Prop -> PSModule
 createFooPSModule primaryProps =
@@ -58,3 +77,15 @@ createFooPSModule primaryProps =
     }
   , subComponents: []
   }
+
+importType :: String -> String -> { mod :: String, name :: PSDeclName }
+importType mod name = { mod: "Untagged.Union", name: (PSDeclNameType { name, includeConstructors: false }) }
+
+shouldContainImport :: Array PSImport -> { mod :: String, name :: PSDeclName } -> Aff Unit
+shouldContainImport imports { mod, name } =
+  containsImport `shouldEqual` true
+
+  where
+    containsImport :: Boolean
+    containsImport =
+      isJust $ Array.find (\i -> i.mod == mod && Array.elem name i.names) imports
